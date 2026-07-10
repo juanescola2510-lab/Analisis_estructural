ef6e6940e02196a91477a50b5b643e519c6ffc01
juanescola2510-lab <juan.escola2510@gmail.com>
2026-07-10 11:00:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 import openpyxl
 from io import BytesIO
+import plotly.graph_objects as go
+import plotly.express as px
 
-st.title("Dashboard Salud Equipos")
+st.set_page_config(
+    page_title="Dashboard Salud Equipos",
+    page_icon="⚙️",
+    layout="wide"
+)
+
+st.title("⚙️ Dashboard Predictivo")
 
 archivo = st.file_uploader(
-    "Seleccione Excel",
+    "Seleccione archivo Excel",
     type=["xlsx"]
 )
 
@@ -22,16 +30,92 @@ if archivo:
 
         datos = list(ws.values)
 
-        encabezados = datos[0]
+        encabezados = list(datos[0])
 
-        registros = datos[1:]
+        registros = list(datos[1:])
 
-        st.success("✅ Datos cargados")
+        idx_equipo = encabezados.index("EQUIPO")
+        idx_punto = encabezados.index("PUNTO DE MEDICIÓN")
 
-        st.write("Encabezados:")
+        idx_u = encabezados.index("ESTADO U")
+        idx_v = encabezados.index("ESTADO V")
+        idx_t = encabezados.index("ESTADO T")
+        idx_e = encabezados.index("ESTADO E")
 
-        st.write(encabezados)
+        equipos = sorted(
+            list(
+                set(
+                    fila[idx_equipo]
+                    for fila in registros
+                )
+            )
+        )
 
-        st.write("Primeras filas:")
+        equipo = st.selectbox(
+            "Seleccione Equipo",
+            equipos
+        )
 
-        st.write(registros[:10])
+        datos_equipo = [
+            fila
+            for fila in registros
+            if fila[idx_equipo] == equipo
+        ]
+
+        salud = sum(
+            fila[idx_e]
+            for fila in datos_equipo
+        ) / len(datos_equipo)
+
+        if salud >= 0.9:
+            estado = "🟢 NORMAL"
+            color = "green"
+
+        elif salud >= 0.7:
+            estado = "🟡 ALARMA"
+            color = "orange"
+
+        else:
+            estado = "🔴 INTERVENIR"
+            color = "red"
+
+        punto_critico = min(
+            datos_equipo,
+            key=lambda x: x[idx_e]
+        )
+
+        c1, c2, c3 = st.columns(3)
+
+        with c1:
+            st.metric(
+                "SALUD DEL EQUIPO",
+                f"{salud:.0%}"
+            )
+
+        with c2:
+            st.metric(
+                "ESTADO",
+                estado
+            )
+
+        with c3:
+            st.metric(
+                "PUNTO MÁS CRÍTICO",
+                punto_critico[idx_punto]
+            )
+
+        # ----------------------
+        # VELOCIMETRO
+        # ----------------------
+
+        fig_gauge = go.Figure(
+            go.Indicator(
+                mode="gauge+number",
+                value=salud * 100,
+
+                number={
+                    "suffix":"%"
+                },
+
+                gauge={
+                    "axis":{
