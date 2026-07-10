8d2a6782787d024c5cd79dc7665aef33bec93e77
juanescola2510-lab <juan.escola2510@gmail.com>
2026-07-10 10:42:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,34 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Prueba")
+st.title("Prueba Lectura Excel")
 
 archivo = st.file_uploader(
-    "Sube Excel",
+    "Selecciona un archivo Excel",
     type=["xlsx"]
 )
 
 if archivo is not None:
 
-    df = pd.read_excel(
-        archivo,
-        engine="openpyxl"
-    )
+    if st.button("INICIAR"):
 
-    st.write(df.head())
+        try:
+
+            df = pd.read_excel(
+                archivo,
+                engine="openpyxl"
+            )
+
+            st.success("✅ Excel leído correctamente")
+
+            st.write("Columnas detectadas:")
+
+            st.write(df.columns.tolist())
+
+            st.write("Primeras filas:")
+
+            st.dataframe(df.head())
+
+        except Exception as e:
+
+            st.error(f"Error al leer el Excel: {e}")
